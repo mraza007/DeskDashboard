ee6e009ec145c09512ffc60ba847fb7708d95b42
mraza007 <muhammadraza0047@gmail.com>
2019-06-27 15:49:05 -0500

    fixed

--- index.py
+++ index.py
@@ -8,30 +8,30 @@ app = Flask(__name__)
 #GPIO2 -> SDA
 #GPIO3 -> SCL
 
-# #Import the Library Requreid 
-# import smbus
-# import time
+#Import the Library Requreid 
+import smbus
+import time
 
-# # for RPI version 1, use "bus = smbus.SMBus(0)"
-# bus = smbus.SMBus(1)
+# for RPI version 1, use "bus = smbus.SMBus(0)"
+bus = smbus.SMBus(1)
 
-# # This is the address we setup in the Arduino Program
-# #Slave Address 1
-# address = 0x66
+# This is the address we setup in the Arduino Program
+#Slave Address 1
+address = 0x66
 
-# #Slave Address 2
-# address_2 = 0x66
+#Slave Address 2
+address_2 = 0x66
 
-# def writeNumber(value):
-#     bus.write_byte(address, value)
-#     bus.write_byte(address_2, value)
-#     # bus.write_byte_data(address, 0, value)
-#     return -1
+def writeNumber(value):
+    bus.write_byte(address, value)
+    bus.write_byte(address_2, value)
+    # bus.write_byte_data(address, 0, value)
+    return -1
 
-# def readNumber():
-#     # number = bus.read_byte(address)
-#     number = bus.read_byte_data(address, 1)
-#     return number
+def readNumber():
+    # number = bus.read_byte(address)
+    number = bus.read_byte_data(address, 1)
+    return number
     
 # while True:
 #     #Receives the data from the User
